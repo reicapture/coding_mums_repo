0936cf8ece1001a7f7ebcddd65a1eb1d743776ca
reicapture <renata.maringka@gmail.com>
2016-04-11 00:40:51 +0700

    contact dan read more

---
+++ js/script.js
@@ -0,0 +1,9 @@
+﻿
+	function readMore() {
+		var temp = document.getElementById("readMore").style.display;
+		if(temp=="none")
+			document.getElementById("readMore").style.display = "block";
+		else
+			document.getElementById("readMore").style.display = "none";
+	}
+
