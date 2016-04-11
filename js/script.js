6704f77cbeb7573718ee138229905f8cd4d393e5
reicapture <renata.maringka@gmail.com>
2016-04-11 09:53:47 +0700

    edit js coba2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
-﻿
-	function readMore() {
-		var temp = document.getElementById("readMore").style.display;
-		if(temp=="none")
-			document.getElementById("readMore").style.display = "block";
-		else
-			document.getElementById("readMore").style.display = "none";
-	}
+﻿function readMore() {
+	var temp = document.getElementById("readMore").style.display;
+	if(temp=="none")
+		document.getElementById("readMore").style.display = "block";
+	else
+		document.getElementById("readMore").style.display = "none";
+}
 
+
+function readMore1() {
+	var temp = document.getElementById("readMore1").style.display;
+	if(temp=="none")
+		document.getElementById("readMore1").style.display = "block";
+	else
+		document.getElementById("readMore1").style.display = "none";
+}
+
+
+function readMore2() {
+	var temp = document.getElementById("readMore2").style.display;
+	if(temp=="none")
+		document.getElementById("readMore2").style.display = "block";
+	else
+		document.getElementById("readMore2").style.display = "none";
+}
+
+
+//
+function readMore2() {
+		var temp = document.getElementsByName
+//
